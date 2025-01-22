863f384a10d8f5993c158a0f1e689a5de31408d9
JayMar921 <jayharronabejar@gmail.com>
2025-01-23 00:49:30 +0800

    Custom Hook and MapGenerator usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useCanvas } from "./hooks/useCanvas";
+import { MapGenerator } from "./handlers/MapGenerator";
 
 function App() {
-  return <></>;
+  const canvasScreen = useCanvas("canvas-screen", 300, 300, "blue");
+
+  function handleClick(clickEvent) {
+    console.log(clickEvent);
+  }
+
+  useEffect(() => {
+    if (!canvasScreen) return;
+    canvasScreen.enableScreenDrag(true);
+    canvasScreen.handleScreenClickedEvent(handleClick);
+
+    const worldGen = new MapGenerator(16, 16, 16);
+    worldGen.generate();
+    worldGen.loadWorld(canvasScreen);
+  }, [canvasScreen]);
+
+  return (
+    <>
+      <div className="content-center h-screen">
+        <canvas className="m-auto" id="canvas-screen"></canvas>
+      </div>
+    </>
+  );
 }
 
 export default App;
