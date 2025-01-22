e4caff4fc0af09c85d0a853ff8295d96faa77b69
JayMar921 <jayharronabejar@gmail.com>
2025-01-23 00:47:55 +0800

    Added Predefined Block and Textures

---
+++ src/materials/Blocks.js
@@ -0,0 +1,28 @@
+import cobblestoneImg from "../assets/textures/blocks/cobblestone.png";
+import dirtImg from "../assets/textures/blocks/dirt_2.png";
+import grassImg from "../assets/textures/blocks/grass_2.png";
+import stoneImg from "../assets/textures/blocks/stone_2.png";
+
+
+export const Blocks = {
+    Cobbledstone: {
+        id: 1,
+        img: cobblestoneImg,
+        name: "Cobbledstone"
+    },
+    Dirt: {
+        id: 2,
+        img: dirtImg,
+        name: "Dirt"
+    },
+    Grass: {
+        id: 3,
+        img: grassImg,
+        name: "Grass"
+    },
+    Stone: {
+        id: 4,
+        img: stoneImg,
+        name: "Stone"
+    },
+}
