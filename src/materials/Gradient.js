c14b4164e477d7240bfd10bc72fbd6bab9c25048
JayMar921 <jayharronabejar@gmail.com>
2025-01-23 01:52:11 +0800

    Gradient Textures

---
+++ src/materials/Gradient.js
@@ -0,0 +1,23 @@
+import one from "../assets/textures/gradient/1.png"
+import two from "../assets/textures/gradient/2.png";
+import three from "../assets/textures/gradient/3.png";
+import four from "../assets/textures/gradient/4.png";
+import five from "../assets/textures/gradient/5.png";
+import six from  "../assets/textures/gradient/6.png";
+import seven from "../assets/textures/gradient/7.png";
+import eight from "../assets/textures/gradient/8.png";
+import nine from "../assets/textures/gradient/9.png";
+import ten from "../assets/textures/gradient/10.png";
+
+export const Gradient = {
+    1: one,
+    2: two,
+    3: three,
+    4: four,
+    5: five,
+    6: six,
+    7: seven,
+    8: eight,
+    9: nine,
+    10: ten,
+}
