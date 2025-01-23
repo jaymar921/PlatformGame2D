5edd117d20e661ba453d13e6f15f8b870bf5a79f
JayMar921 <jayharronabejar@gmail.com>
2025-01-24 00:52:47 +0800

    WorldGenV2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "./App.css";
 import { useCanvas } from "./hooks/useCanvas";
 import { MapGenerator } from "./handlers/MapGenerator";
+import { MapGeneratorV2 } from "./handlers/MapGenerator_v2";
 
 function App() {
   const canvasScreen = useCanvas(
@@ -19,10 +20,13 @@ function App() {
     if (!canvasScreen) return;
     canvasScreen.enableScreenDrag(true);
     canvasScreen.handleScreenClickedEvent(handleClick);
-    canvasScreen.setCameraOffset(0, 0);
-    const worldGen = new MapGenerator(5, 5, 16, "map-2", 1.9);
+    canvasScreen.setCameraOffset(-128, -128);
+    const worldGen = new MapGenerator(10, 10, 16, "map-2", 6);
     worldGen.generate();
     worldGen.loadWorld(canvasScreen);
+
+    // const worldGen2 = new MapGeneratorV2(30, 30, 1, "map-1");
+    // worldGen2.generateMap(canvasScreen);
   }, [canvasScreen]);
 
   return (
