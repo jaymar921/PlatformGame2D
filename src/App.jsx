cfe470b4086db808e3f6a5c3511baf0ebd1a6c6e
JayMar921 <jayharronabejar@gmail.com>
2025-01-31 00:22:14 +0800

    PlayerMoveEvent custom collision implementation

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { MapGeneratorV2 } from "./handlers/MapGenerator_v2";
 import { LevelGenerator } from "./handlers/LevelGenerator";
 import { TextureMap } from "./contants/TextureMap";
 import { level1 } from "./contants/Levels";
-import map1Image from "./assets/textures/map/map-1-tiles.png";
+import map1Image from "./assets/textures/map/map-1-tiles-v2.png";
 import TilemapLevelGenerator from "./handlers/TilemapLevelGenerator";
 import { TileMapColliders } from "./contants/TileMapColliders";
 import PlayerHandler from "./handlers/PlayerHandler";
@@ -18,14 +18,26 @@ function App() {
     window.innerHeight,
     "#020617"
   );
+  const [handlersLoaded, setHandlersLoaded] = useState(false);
+  const [modal, setModal] = useState({ show: false, display: "" });
 
   function handleClick(clickEvent) {}
 
+  function onPlayerCollisionEvent(event) {
+    // check if player collide with object
+    if (event.collisionId === "id-2") {
+      setModal({ show: true, display: "Paper" });
+    } else if (event.collisionId === "id-3") {
+      setModal({ show: true, display: "Stone" });
+    } else if (event.collisionId === "id-4") {
+      setModal({ show: true, display: "Pothole" });
+    } else {
+      setModal({ show: false, display: "" });
+    }
+  }
+
   useEffect(() => {
     if (!canvasScreen) return;
-    canvasScreen.enableScreenDrag(true);
-    canvasScreen.handleScreenClickedEvent(handleClick);
-    canvasScreen.setCameraOffset(-20, -20);
     // const worldGen = new MapGenerator(10, 10, 16, "map-2", 6);
     // worldGen.generate();
     // worldGen.loadWorld(canvasScreen);
@@ -35,27 +47,41 @@ function App() {
 
     // const levelGenerator = new LevelGenerator(canvasScreen, TextureMap, 16);
     // levelGenerator.loadLevel(level1);
-    const tilemapLevelGen = new TilemapLevelGenerator(canvasScreen);
-    const playerHandler = new PlayerHandler(canvasScreen, {
-      x: 8 * 32,
-      y: 5 * 32,
-    });
+    if (!handlersLoaded) {
+      canvasScreen.enableScreenDrag(true);
+      canvasScreen.handleScreenClickedEvent(handleClick);
+      canvasScreen.setCameraOffset(-20, -20);
 
-    tilemapLevelGen.addLevel(
-      map1Image,
-      512,
-      320,
-      { x: 0, y: 0 },
-      TileMapColliders.level1
-    );
+      const tilemapLevelGen = new TilemapLevelGenerator(canvasScreen);
 
-    tilemapLevelGen.loadLevel(1);
+      tilemapLevelGen.addLevel(
+        map1Image,
+        512,
+        320,
+        { x: 0, y: 0 },
+        TileMapColliders.level1
+      );
 
-    playerHandler.addColliders(tilemapLevelGen.getBoxCollider(1));
+      tilemapLevelGen.loadLevel(1);
+
+      const playerHandler = new PlayerHandler(canvasScreen, {
+        x: 8 * 32,
+        y: 5 * 32,
+      });
+
+      playerHandler.addColliders(tilemapLevelGen.getBoxCollider(1));
+      playerHandler.addPlayerMoveEvent(onPlayerCollisionEvent);
+      setTimeout(() => setHandlersLoaded(true), 200);
+    }
   }, [canvasScreen]);
 
   return (
     <>
+      {modal.show && (
+        <div className="absolute border-2 left-[50%] translate-x-[-50%] top-[10%] p-2 rounded-md bg-gray-200 text-slate-900 shadow-xl">
+          <h1 className="font-bold">{modal.display}</h1>
+        </div>
+      )}
       <div className="content-center h-screen">
         <canvas className="m-auto" id="canvas-screen"></canvas>
       </div>
